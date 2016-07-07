b5448561ceb413e917659148f4c97ba82a752ed0
Synthetic Monad <synth@neurion.ai>
2016-07-07 09:20:35 +0200

    Big Bang

---
+++ SwiftOpenGL/SwiftOpenGL/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftOpenGL
+//
+//  Created by Pawel Ropa on 07/07/16.
+//  Copyright © 2016 Pawel Ropa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftOpenGL/SwiftOpenGL/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftOpenGL
+//
+//  Created by Pawel Ropa on 07/07/16.
+//  Copyright © 2016 Pawel Ropa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
