0b2775838951e98232b946274a01c9537f73cc48
Synthetic Monad <synth@neurion.ai>
2016-07-15 08:07:39 +0200

    New project setup

---
+++ SwiftOpenGL/FirstVertex/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  FirstVertex
+//
+//  Created by Pawel Ropa on 15/07/16.
+//  Copyright © 2016 Pawel Ropa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+}
+
